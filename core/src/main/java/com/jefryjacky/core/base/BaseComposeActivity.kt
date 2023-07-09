d71e86a1d62d6bb4dbf7f8f2b3f0491309bf4767
Jefry Jacky <jefryjacky@gmail.com>
2023-07-09 18:32:51 +0700

    base compose activity

---
+++ core/src/main/java/com/jefryjacky/core/base/BaseComposeActivity.kt
@@ -0,0 +1,49 @@
+package com.jefryjacky.core.base
+
+import android.content.Context
+import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import com.jefryjacky.core.R
+import com.jefryjacky.core.di.ViewModelFactory
+import javax.inject.Inject
+
+abstract class BaseComposeActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelFactory
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    protected fun initObserver(viewModel:BaseViewModel){
+        viewModel.unauthorizedEvent.observe(this) {
+            it.contentIfNotHaveBeenHandle?.let {
+                unauthorized()
+            }
+        }
+        viewModel.unknownErrorEvent.observe(this) {
+            it.contentIfNotHaveBeenHandle?.let {
+                unknownError(it)
+            }
+        }
+    }
+
+    protected fun hideKeyBoard(){
+        this.currentFocus?.let { view ->
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            imm?.hideSoftInputFromWindow(view.windowToken, 0)
+        }
+    }
+
+    protected open fun unauthorized(){
+        // no implementation
+    }
+
+    protected open fun unknownError(message:String){
+        Toast.makeText(this, getString(R.string.opps_something_wrong), Toast.LENGTH_SHORT).show()
+    }
+}
